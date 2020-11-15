020635ce865c3515f68dc38f950bb41567216573
OmarLa98 <omarlopez.789@gmail.com>
2020-11-15 17:47:44 -0600

    Se genera imagenes en sponsors y modificaciones de colores y giroscopio

--- app/src/main/java/com/flimflo/alpha/ui/sponsors/SponsorsFragment.kt
+++ app/src/main/java/com/flimflo/alpha/ui/sponsors/SponsorsFragment.kt
@@ -15,23 +15,19 @@ class SponsorsFragment : Fragment() {
         Sponsors("Tel 1: 01 (474) 746 7076\n" + "Tel 2: 01 (474) 746 7077\n" + "correo: recepcion@eml.com.mx","Carr. Lagos-San Juan km 3 S/N\n" +
                 "Col. Jacales\n" +
                 "CP: 47470\n" +
-                "Lagos de Moreno, Jal."),
+                "Lagos de Moreno, Jal.", R.drawable.eml_logo),
         Sponsors("Tel: 01 (474) 742 8199","Calle Canal #390\n" +
                 "Col. Granadillas\n" +
-                "CP: 47433"),
+                "CP: 47433", R.drawable.kikapu_logo),
         Sponsors("Tel: 01 (474) 738 8716","Emiliano Zapata #325 A\n" +
                 "Col. Centro\n" +
                 "CP: 47400\n" +
-                "Lagos de Moreno, Jal."),
+                "Lagos de Moreno, Jal.", R.drawable.fisio_logo),
         Sponsors("Tel: 474 108 2758\n" + "correo: recepcion@eml.com.mx","Calle Francisco I.Madero #484 timbre 4\n" +
                 "Col. Centro\n" +
                 "CP: 47400\n" +
-                "Lagos de Moreno, Jal."),
-        Sponsors("Tel: 01 (474) 742 4536\n" +
-                "correo: ventas@sicc-bajio.com\n" + "correo: recepcion@eml.com.mx","Calle Limón #48\n" +
-                "Col. Huertos Familiares San Pedro\n" +
-                "CP: 47472\n" +
-                "Lagos de Moreno, Jal."))
+                "Lagos de Moreno, Jal.", R.drawable.nutrisana_logo)
+       )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
